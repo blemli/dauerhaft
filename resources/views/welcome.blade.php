579ec04a281a5504f32f3e32528bb9ca49e5bafe
grafst <grafst@users.noreply.github.com>
2024-10-10 20:08:48 +0200

    tailwind from cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="favicon.svg">
+	 <script src="https://cdn.tailwindcss.com"></script>
         @vite('resources/css/app.css')
 
         <title>Dauerhaft</title>
