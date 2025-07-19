f5ba3c9ad46ecfbfd4e3794f187fc348e894a24c
grafst <grafst@users.noreply.github.com>
2025-07-19 20:52:05 +0200

    add heartbeat /up

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome', [
-        'things' => Thing::all()->sortBy('months_alive',descending: true)
+        'things' => Thing::public()->get()->sortBy('months_alive',descending: true)
     ]);
+});
+
+Route::get('/up', function () {
+    return response('OK', 200);
 });
